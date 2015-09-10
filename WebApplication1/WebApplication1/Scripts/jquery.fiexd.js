eb271209b926e4d924db1c24a72d3d5d6367b3eb
coasline <120993289@qq.com>
2015-09-10 15:54:30 +0800

    test
    test

---
+++ WebApplication1/WebApplication1/Scripts/jquery.fiexd.js
@@ -0,0 +1,27 @@
+﻿(function ($) {
+    var _options = {};
+    jQuery.fn.fiexd = function (options) {
+        var id = $(this).attr("id");
+        _options[id] = $.extend({}, $.fn.fiexd.defaults, options);
+        var obj = $(this);
+        var offsetTop = this.offset().top - parseInt(_options[id].top);
+        _scroll($(document).scrollTop() > offsetTop);
+        $(window).scroll(function () {
+            _scroll($(document).scrollTop() > offsetTop);
+        });
+        function _scroll(isChange) {
+            if (isChange) {
+                if ('undefined' == typeof (document.body.style.maxHeight)) {
+                    obj.css({ "position": "absolute", "top": $(document).scrollTop() + parseInt(_options[id].top) });
+                } else {
+                    obj.css({ "position": "fixed", "top": _options[id].top });
+                }
+            } else {
+                obj.css({ "position": "", "top": "" });
+            }
+        }
+    };
+    jQuery.fn.fiexd.defaults = {
+        top: '0px'
+    };
+})(jQuery);
---
+++ WebApplication1/WebApplication1/Scripts/jquery.iframe-autoheight.js
@@ -0,0 +1,57 @@
+﻿jQuery.fn.extend(
+{
+    iframeAutoHeight: function (callback, stayInSecond) {
+        this.each(function () {
+            var w = this;
+            $(this).load(function () {
+                var cnt = 0;
+                var lastHeight = 0;
+                setTimeout(function () {
+                    try {
+                        //var bHeight = w.contentWindow.document.body.offsetHeight;
+                        //var dHeight = w.contentWindow.document.documentElement.offsetHeight;
+                        //var contentHeight = Math.max(bHeight, dHeight);
+
+                        //var bWidth = w.contentWindow.document.body.offsetWidth;
+                        //var dWidth = w.contentWindow.document.documentElement.offsetWidth;
+                        //var contentWidth = Math.max(bWidth, dWidth);
+
+                        //var contentHeight = 0;
+                        //var contentWidth = 0;
+                        //if (w.contentDocument && w.contentDocument.body.offsetHeight) {
+                        //    contentHeight = w.contentDocument.body.offsetHeight;
+                        //    contentWidth = w.contentDocument.body.offsetWidth;
+                        //} else if (w.Document && w.Document.body.scrollHeight) {
+                        //    contentHeight = w.Document.body.scrollHeight;
+                        //    contentWidth = w.Document.body.scrollWidth;
+                        //}
+
+                        var contentHeight = 0;
+                        var contentWidth = 0;
+                        var isChrome = navigator.userAgent.toLowerCase().match(/chrome/) != null;
+                        if (isChrome) {
+                            contentHeight = w.contentWindow.document.documentElement.scrollHeight;
+                            contentWidth = w.contentWindow.document.documentElement.scrollWidth;
+                        }
+                        else {
+                            contentHeight = w.contentWindow.document.body.scrollHeight;
+                            contentWidth = w.contentWindow.document.body.scrollWidth;
+                        }
+
+                        if (lastHeight != contentHeight) {
+                            $(w).css("height", contentHeight);
+                            lastHeight = contentHeight;
+                            if (callback) callback(lastHeight, w.contentDocument ? w.contentDocument : w.Document);
+                        }
+                        if ((contentWidth - $(w).width()) > 20) {
+                            $(w).width(contentWidth);
+                        }
+                    } catch (e) {
+
+                    }
+                    if (++cnt < ((stayInSecond || 3600) * 2)) setTimeout(arguments.callee, 500);
+                }, 100);
+            });
+        });
+    }
+});
